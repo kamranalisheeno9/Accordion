d355fc97c0bc9c8276f0ff70527c13d81763eb52
kamranalisheeno9 <kamranalisheeno9@gmail.com>
2022-08-31 19:55:46 +0500

    Finalized Modified

--- src/components/checkboxComp.js
+++ src/components/checkboxComp.js
@@ -1,138 +1,124 @@
 import React, { useEffect, useState } from "react";
-import { Checkbox } from "@mui/material";
-import FormControl from "@mui/material/FormControl";
-import FormGroup from "@mui/material/FormGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-
-
-const EuropeData = [
-  { name: "Rome" },
-  { name: "Paris" },
-  { name: "Kiev" },
-];
-const Countries = ["Brazil", "Kenya"];
-
+import Checkbox from "./Checkbox";
+import { Countries } from "./data";
 
 const CheckboxComp = () => {
+  const [isCheckAll, setIsCheckAll] = useState(false);
+  const [isCheck, setIsCheck] = useState([]);
   const [Europe, setEurope] = useState([]);
-  const [country, setCountry] = useState([]);
-  
-  
-  
+  const [Country, setCountry] = useState([]);
+
+  const EuropeData = Countries.filter((country) => {
+    if (country.continent === "Europe") {
+      return country;
+    }
+  });
+
+  const OtherData = Countries.filter((country) => {
+    if (country.continent !== "Europe") {
+      return country;
+    }
+  });
+
   useEffect(() => {
     setEurope(EuropeData);
+    setCountry(OtherData);
   }, []);
-  
-  
-  const handleChange = (e) => {
+
+
+  const handleSelectAll = (e) => {
+    setIsCheckAll(!isCheckAll);
+    setIsCheck(Europe.map((li) => li.name));
+    if (isCheckAll) {
+      setIsCheck([]);
+    }
+    
+  };
+  console.log(isCheck);
+
+  const handleClick = (e) => {
+
     const { name, checked } = e.target;
-    if (name === "europe") {
-      let tempUser = Europe.map((user) => {
-        return { ...user, isChecked: checked };
-      });
-      setEurope(tempUser);
-    } else {
-      let tempUser = Europe.map((user) =>
-        user.name === name ? { ...user, isChecked: checked } : user
-      );
-      setEurope(tempUser);
+    setIsCheck([...isCheck, name]);
+    if (!checked) {
+      setIsCheck(isCheck.filter((item) => item !== name));
     }
-    console.log(e.target.name);
-
   };
 
-  
-  const getValue = (e) => {
-    const countryNames = country;
-    if (e.target.checked == true) {
-      countryNames.push(e.target.name);
-      setCountry(countryNames);
-    }
-    console.log(e.target.name);
-  };
+  const catalog = Europe.map(({ id, name }) => {
+    return (
+      <>
+        <li style={{ listStyle: "none", fontSize: "17px", lineHeight: "30px" }}>
+          <Checkbox
+            key={id}
+            type="checkbox"
+            name={name}
+            id={id}
+            handleClick={handleClick}
+            isChecked={isCheck.includes(name)}
+          />
+          {name}
+        </li>
+      </>
+    );
+  });
+  const catalog2 = Country.map(({ id, name }) => {
+    return (
+      <>
+        <li style={{ listStyle: "none", fontSize: "17px", lineHeight: "30px" }}>
+          <Checkbox
+            key={id}
+            type="checkbox"
+            name={name}
+            id={id}
+            handleClick={handleClick}
+            isChecked={isCheck.includes(name)}
+          />
 
-  
+          {name}
+        </li>
+      </>
+    );
+  });
+
   return (
-    <div className="MainComp" style={{ textAlign: "left" }}>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Pick your destination</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <FormControl component="fieldset" variant="standard">
-            {Countries.map((countries, key) => {
-              return (
-                <FormGroup key={key}>
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        name={countries}
-                        onChange={(e) => getValue(e)}
-                      />
-                    }
-                    label={countries}
-                  />
-                </FormGroup>
-              );
-            })}
-          </FormControl>
-        </AccordionDetails>
+    <div>
+      <div className="MainComp" style={{ textAlign: "left" }}>
         <Accordion>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1a-content"
             id="panel1a-header"
           >
-            <Typography>
-              <FormControl component="fieldset" variant="standard">
-                <FormGroup>
-                  <FormControlLabel
-                    control={
-                      <Checkbox
-                        style={{ marginLeft: "20px" }}
-                        name="europe"
-                
-                        checked={!Europe.some((user) => user?.isChecked !== true)}
-                        onChange={handleChange}
-                      />
-                    }
-                    label="Europe"
-                  />
-                </FormGroup>
-              </FormControl>
-            </Typography>
+            <Typography>Pick your destination</Typography>
           </AccordionSummary>
-          <AccordionDetails>
-            <FormControl component="fieldset" variant="standard">
-              {Europe.map((user, index) => {
-                return (
-                  <FormGroup key={index}>
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                        name={user.name}
-                        checked={user?.isChecked || false}
-                        onChange={handleChange}
-                        />
-                      }
-                      label={user.name}
-                    />
-                  </FormGroup>
-                );
-              })}
-            </FormControl>
-          </AccordionDetails>
+          <AccordionDetails>{catalog2}</AccordionDetails>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>
+                <Checkbox
+                  type="checkbox"
+                  name="selectAll"
+                  id="selectAll"
+                  handleClick={handleSelectAll}
+                  isChecked={isCheckAll}
+                />
+                Europe
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>{catalog}</AccordionDetails>
+          </Accordion>
         </Accordion>
-      </Accordion>
+      </div>
     </div>
   );
 };
---
+++ src/components/data.js
@@ -0,0 +1,29 @@
+export const Countries = [
+    {
+      id: "01",
+      name: "Brazil",
+      continent:"South America"
+    },
+    {
+      id: "02",
+      name: "Kenya",
+      continent:"Africa"
+    },
+    {
+      id: "03",
+      name: "Rome",
+      continent:"Europe"
+    },
+    {
+      id: "04",
+      name: "Paris",
+      continent:"Europe"
+    },
+    {
+      id: "05",
+      name: "Kiev",
+      continent:"Europe"
+    },
+    
+  ];
+  
